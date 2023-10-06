5da7b356db4faa481ce148e08487002785f99fbc
Thomas PRADEAU <pradeau.thomas0@gmail.com>
2023-10-06 17:21:36 +0200

    merge: home view

--- src/fr/thomas/proto0/controller/GameController.java
+++ src/fr/thomas/proto0/controller/GameController.java
@@ -1,12 +1,16 @@
 package fr.thomas.proto0.controller;
 
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import fr.thomas.proto0.model.Game;
 import fr.thomas.proto0.model.Player;
 import fr.thomas.proto0.model.Question;
+import fr.thomas.proto0.utils.DatabaseHelper;
 import fr.thomas.proto0.utils.QuestionsBuilder;
 import fr.thomas.proto0.view.ConsoleView;
+import fr.thomas.proto0.view.HomeView;
 import fr.thomas.proto0.view.LoginView;
 
 public class GameController {
@@ -18,6 +22,7 @@ public class GameController {
 	private int diff;
 
 	private LoginView loginView;
+	private HomeView homeView;
 
 	/**
 	 * @author Thomas PRADEAU
@@ -27,32 +32,36 @@ public class GameController {
 		this.view = new ConsoleView(this);
 		this.loginView = new LoginView(this);
 		player = new Player("", this);
+		this.homeView = new HomeView(this);
 		this.loginView.setVisible(true);
-
 	}
 
 	public void playerAuth(String name, String password) {
 		if (player.authenticate(name, password)) {
-			this.loginView.setVisible(false);	
-
-			diff = view.askDifficulty("Choisissez la difficulté :");
-
-			// questions = QuestionsBuilder.readQuestions(this,
-			// "resources/data/questions.txt"); //Stubdata questions
-			questions = QuestionsBuilder.loadQuestions(this, diff);
-
-			// Créer la game avec joueur et questions
+			this.loginView.setVisible(false);
+			this.homeView.setVisible(true);
 			this.game = new Game(this, player);
-			game.insert();
-
-			game.getRandomQuestions(2); // Choisir les questions aléatoirement
-			game.begin();
-
-			game.insert();
+			this.homeView.updatePlayerData(player.getName(), game.getHighestScore(player));
 		} else {
 			System.err.println("Invalid password...");
 		}
 	}
+
+	public void startGame() {
+		diff = view.askDifficulty("Choisissez la difficulté :");
+		questions = QuestionsBuilder.loadQuestions(this, diff);
+
+		//TODO Déduire nb de questions choisies aleatoirement
+		game.getRandomQuestions(2); // Choisir les questions aléatoirement
+		game.begin();
+
+		//Partie terminee
+		
+		game.insert();
+		this.homeView.updatePlayerData(player.getName(), game.getHighestScore(player));
+	}
+	
+	
 
 	public ConsoleView getView() {
 		return view;
---
+++ src/fr/thomas/proto0/view/HomeView.java
@@ -0,0 +1,60 @@
+package fr.thomas.proto0.view;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import fr.thomas.proto0.controller.GameController;
+
+public class HomeView extends JFrame {
+
+	private GameController controller;
+
+	private JLabel usernameLabel;
+	private JLabel scoreLabel;
+
+	public HomeView(GameController controller) {
+		getContentPane().setLayout(null);
+
+		JLabel lblBestScore = new JLabel("Meilleur score : ");
+		lblBestScore.setBounds(12, 12, 115, 17);
+		getContentPane().add(lblBestScore);
+
+		JLabel lblScore = new JLabel("");
+		lblScore.setBounds(106, 12, 111, 17);
+		getContentPane().add(lblScore);
+		this.scoreLabel = lblScore;
+
+		JLabel lblBienvenue = new JLabel("Bienvenue");
+		lblBienvenue.setBounds(27, 72, 85, 17);
+		getContentPane().add(lblBienvenue);
+
+		JLabel lblUsername = new JLabel("");
+		lblUsername.setBounds(106, 72, 141, 17);
+		getContentPane().add(lblUsername);
+		this.usernameLabel = lblUsername;
+
+		JButton btnJoueur = new JButton("Jouer");
+		btnJoueur.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				controller.startGame();
+			}
+		});
+		btnJoueur.setBounds(140, 178, 105, 27);
+		getContentPane().add(btnJoueur);
+		setSize(400, 300);
+		this.controller = controller;
+	}
+
+	public void updatePlayerData(String name, int score) {
+		this.usernameLabel.setText(name);
+		this.scoreLabel.setText(Integer.toString(score));
+	}
+
+	private static final long serialVersionUID = -912987546852985245L;
+
+}
