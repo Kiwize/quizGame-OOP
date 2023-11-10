9780a4929be9b19b79c7afd273a60eea6624e027
Thomas PRADEAU <pradeau.thomas0@gmail.com>
2023-11-10 15:35:18 +0100

    feat: password change view and db update password strength verification

--- src/fr/thomas/proto0/utils/DatabaseHelper.java
+++ src/fr/thomas/proto0/utils/DatabaseHelper.java
@@ -1,7 +1,6 @@
 package fr.thomas.proto0.utils;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
