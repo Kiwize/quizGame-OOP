b28c30444de0fd9b7a3b519f2cf94ccefef86bb0
Thomas PRADEAU <thomas.pradeau0@gmail.com>
2024-03-29 13:47:44 +0100

    feat: listen server request for player activity

--- src/fr/thomas/proto0/net/request/Login.java
+++ src/fr/thomas/proto0/net/request/Login.java
@@ -3,15 +3,22 @@ package fr.thomas.proto0.net.request;
 import fr.thomas.proto0.net.object.PlayerNetObject;
 
 public class Login {
-	
+
 	public static class LoginRequest {
 		public String username;
 		public String password;
 	}
-	
+
 	public static class LoginResponse {
 		public boolean isConnected;
 		public PlayerNetObject player;
 	}
 
+	public static class PlayerActivityRequest {
+
+	}
+
+	public static class PlayerActivityAnswer {
+		public boolean isPlayerActive;
+	}
 }
